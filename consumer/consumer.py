574fa39353785b9ffb4b8f0d6020d300c104568b
Rochdy-max <rochdy.bachabi@epitech.eu>
2024-11-28 16:16:01 +0100

    move consumer to a standalone service

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,15 +1,20 @@
 from kafka import KafkaConsumer
-import json
+import os, sys, json
+
+def log(*args, **kwargs):
+    print(args, kwargs, file=sys.stderr)
 
 def json_deserializer(data: bytes):
     return json.loads(data.decode('utf-8'))
 
+KAFKA_BROKER = os.environ.get('KAFKA_BROKER')
+
 consumer = KafkaConsumer(
     'test_topic',
-    bootstrap_servers=['localhost:9092'],
+    bootstrap_servers=[KAFKA_BROKER],
     value_deserializer=json_deserializer,
     auto_offset_reset='earliest'
 )
 
 for message in consumer:
-    print(f'Receiving message: {message}')
+    log(f'Receiving message: {message}')
